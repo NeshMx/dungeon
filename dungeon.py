c973fdcf19cbc537e81a068bee47dad3aaf703ad
Huerta, Alejandro (Alex) <212631045@ge.com>
2018-04-18 11:52:46 -0500

    Update

--- dungeon.py
+++ dungeon.py
@@ -1,9 +1,12 @@
 import re
 
+
 class Node:
-    def __init__(self, id, reachable, adj):
+    def __init__(self, id, value, adj):
         self.id = id
-        self.reachable = reachable
+        self.start = True if value == 'S' else False
+        self.end = True if value == 'E' else False
+        self.reachable = True if value == '.' else False
         self.adj = adj
 
 
@@ -14,27 +17,31 @@ class Layer:
 
 class Maze:
 
-    @classmethod
-    def load_maze(cls, fname):
-        with open(fname) as inf:
-            lines = (line.rstrip("\r\n") for line in inf)
-            # maze = [list(line) for line in lines]
-            count = 0
-            for line in list(lines):
-                try:
-                    if re.match('^[-+]?[0-9]+$', line[0]):
-                        
-                        print('Passed')
-                        
-                except:
-                    pass
-                
-
-    def __init__(self, maze):
+    def __init__(self, maze=[]):
         self.maze = maze
 
     def __str__(self):
         return "\n".join(''.join(line) for line in self.maze)
+
+    @classmethod
+    def load_maze(cls, fname):
+        maze_arrays = []
+        with open(fname) as inf:
+            lines = (line.rstrip("\r\n") for line in inf)
+            start_maze = False
+            id = 1
+            for line in list(lines):
+                try:
+                    if re.match('^[-+]?[0-9]+$', line[0]):
+                        tmp_maze = Maze()
+                        id = 0
+                        start_maze = True
+                        continue
+                    if start_maze:
+                        for ch in line:
+                            tmp_node = Node(id+=1, ch, )
+                except:
+                    pass
 
 
 def main():
