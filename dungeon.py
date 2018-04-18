eabe0704b7856e29f84b74645e94f98dfb45e91b
NeshMx <alex.huerta0424@hotmail.com>
2018-04-17 21:31:58 -0500

    Update

--- dungeon.py
+++ dungeon.py
@@ -1,36 +1,22 @@
-# Input: L R C (i.e. 3 4 5)
-# Ouput: Escaped in 11 minute(s)
-
-
 class Maze:
     @classmethod
-    def load_file(self, fname):
-        with open(fname) as fp:
-            # params = fp.read().split(" ")
-            lines = (line.rstrip("\r\n") for line in fp)
+    def load_maze(cls, fname):
+        with open(fname) as inf:
+            lines = (line.rstrip("\r\n") for line in inf)
             maze = [list(line) for line in lines]
-        return self(maze)
-
-        # levels = params[0]
-        # rows = params[1]
-        # columns = params[2]
+        return cls(maze)
 
     def __init__(self, maze):
         self.maze = maze
 
     def __str__(self):
-        return '\n'.join(''.join(line) for line in self.maze)
-
-    def move(self, parameter_list):
-        pass
+        return "\n".join(''.join(line) for line in self.maze)
 
 
 def main():
-    # file_name = input('Introduce la ruta del archivo de entradas: ')
-    maze = Maze().load_file("input.txt")
+    maze = Maze.load_maze("input.txt")
     print(maze)
-    # show_dungeon()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
